f6b2627b7e92034500b76aeecc674c328479bdf7
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-07-07 08:08:59 +0800

    Making some changes on pictures and names

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
+    name: "Mark Jayson Rodelas",
     job: "web developer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      "mjrodelas.jpg",
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, dolorum eos quibusdam ipsam sit nam neque. Magnam dolores ipsa perspiciatis.",
   },
   {
     id: 2,
-    name: "anna johnson",
+    name: "Rose Ann Parales",
     job: "web designer",
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
@@ -20,7 +20,7 @@ const reviews = [
   },
   {
     id: 3,
-    name: "peter jones",
+    name: "Ricky Paterno",
     job: "intern",
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
@@ -29,7 +29,7 @@ const reviews = [
   },
   {
     id: 4,
-    name: "bill anderson",
+    name: "Judy Ann San Guillermo",
     job: "the boss",
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
